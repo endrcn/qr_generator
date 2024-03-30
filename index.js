08864f3711863ab252c477ddd9bef43851db1824
endrcnn <endercan34@gmail.com>
2024-03-31 00:51:59 +0300

    vercel deployment fix

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var qrRouter = require('./routes/qr');
 
-var app = express();
+//Middlewares
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,15 +28,5 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening port ${port}`));
